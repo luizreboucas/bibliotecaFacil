46276ccf11eddfe6a042f54a62d6d8f57dccc077
luizreboucas <primoneto@gmail.com>
2023-11-12 07:40:02 -0300

    CRUD books concluído

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import db from './config/dbConfig';
 import Users from './models/userModel';
 import { env } from '../env'
 import userRoutes from './routes/userRoutes'
+import booksRoutes from './routes/booksRoutes'
 const PORT = env.PORT
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
+app.use(booksRoutes);
 db.on("error", console.log.bind(console, "Erro de conexão"))
 db.once("open", () => {
     console.log("Db connection working")
