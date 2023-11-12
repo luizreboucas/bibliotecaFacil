12c3eee9b2e617550897eacad6a6b5e1b4f04747
luizreboucas <primoneto@gmail.com>
2023-11-12 08:13:26 -0300

    Cruds sells, books e users completos

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Users from './models/userModel';
 import { env } from '../env'
 import userRoutes from './routes/userRoutes'
 import booksRoutes from './routes/booksRoutes'
+import sellsRoutes from './routes/sellsRoutes'
 const PORT = env.PORT
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(booksRoutes);
+app.use(sellsRoutes);
 db.on("error", console.log.bind(console, "Erro de conexão"))
 db.once("open", () => {
     console.log("Db connection working")
